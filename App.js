b7044d456f6f25ca2fa220063455961946e26e09
EstherOA <estherowusuansah19@gmail.com>
2020-06-22 14:41:15 +0000

    modified app.js

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { AppContainer } from './navigators/AppContainer';
+import { View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppContainer />
+      // <AppContainer />
+      <View></View>
     );
   }
 }
